98cc8b596322147a9b67f6510d5dd7d2b5357f96
damhyeong <rhdwhdals8@naver.com>
2023-08-20 13:30:17 +0900

    [unrated] Title: 홀짝 구분하기, Time: 139.78 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        if(n % 2 == 0)
+            bw.write(Integer.toString(n) + " is even");
+        else
+            bw.write(Integer.toString(n) + " is odd");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
