3b5f3a0c36aba8888ae116bbb24ac9974d313403
damhyeong <rhdwhdals8@naver.com>
2023-08-22 19:44:11 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        int index = 0;
+        for(int i = 0; i < n_str.length(); i++){
+            if(n_str.charAt(i) != '0'){
+                index = i;
+                break;
+            }
+        }
+        answer = n_str.substring(index, n_str.length());
+        
+        return answer;
+    }
+}
