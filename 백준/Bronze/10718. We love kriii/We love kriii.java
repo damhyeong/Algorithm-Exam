a94b58288f8a750ae5e8562c6e812cd54043e397
damhyeong <rhdwhdals8@naver.com>
2023-08-26 23:39:02 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("강한친구 대한육군\n강한친구 대한육군");
+		bw.flush();
+		bw.close();
+		
+	}
+	
+}
