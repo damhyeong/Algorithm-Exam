0111e2baf9cb57dffab787a020a328d65380adb3
damhyeong <rhdwhdals8@naver.com>
2023-09-19 11:17:43 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -3,8 +3,14 @@ import java.util.*;
 import java.io.*;
 
 /**
+시험 성적 성공
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	354634	193997	163654	55.039%
 문제
-시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 
+70 ~ 79점은 C, 
+60 ~ 69점은 D, 
+나머지 점수는 F를 출력하는 프로그램을 작성하시오.
 
 입력
 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
@@ -23,20 +29,22 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int score = Integer.parseInt(st.nextToken());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int score = Integer.parseInt(br.readLine());
 		
 		if(score >= 90)
-			System.out.printf("%c", 'A');
+			bw.write("A");
 		else if(score >= 80)
-			System.out.printf("%c", 'B');
+			bw.write("B");
 		else if(score >= 70)
-			System.out.printf("%c", 'C');
+			bw.write("C");
 		else if(score >= 60)
-			System.out.printf("%c", 'D');
+			bw.write("D");
 		else
-			System.out.printf("%c", 'F');
+			bw.write("F");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
