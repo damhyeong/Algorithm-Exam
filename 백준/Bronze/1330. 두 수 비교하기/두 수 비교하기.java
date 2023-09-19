1e073c5e8860c857b4e95a35ead8c921b4a9b8e8
damhyeong <rhdwhdals8@naver.com>
2023-09-19 11:13:41 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -3,6 +3,9 @@ import java.util.*;
 import java.io.*;
 
 /**
+두 수 비교하기 성공
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	512 MB	394228	189986	156717	49.639%
 문제
 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
 
@@ -17,6 +20,18 @@ A가 B보다 작은 경우에는 '<'를 출력한다.
 A와 B가 같은 경우에는 '=='를 출력한다.
 제한
 -10,000 ≤ A, B ≤ 10,000
+예제 입력 1 
+1 2
+예제 출력 1 
+<
+예제 입력 2 
+10 2
+예제 출력 2 
+>
+예제 입력 3 
+5 5
+예제 출력 3 
+==
  */
 
 public class Main {
@@ -24,18 +39,19 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		
-		if(a > b)
-			System.out.printf("%c", '>');
-		else if(a < b)
-			System.out.printf("%c", '<');
-		else 
-			System.out.printf("%s", "==");
-		
+		int num1 = Integer.parseInt(st.nextToken());
+		int num2 = Integer.parseInt(st.nextToken());
+		if(num1 > num2)
+			bw.write(">");
+		else if(num1 == num2)
+			bw.write("==");
+		else if (num1 < num2)
+			bw.write("<");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
