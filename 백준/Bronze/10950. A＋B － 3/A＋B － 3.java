1ab76aa2fd08f67fa3a7e0177bdc177f0c90754d
damhyeong <rhdwhdals8@naver.com>
2023-11-20 03:23:25 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -3,6 +3,10 @@ import java.util.*;
 import java.io.*;
 
 /**
+A+B - 3 성공
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	280038	162884	136267	58.306%
+
 문제
 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
@@ -34,19 +38,18 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int t = Integer.parseInt(st.nextToken());
-		int[][] testCase = new int[2][t];
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < t; i++) {
-			st = new StringTokenizer(br.readLine());
-			testCase[0][i] = Integer.parseInt(st.nextToken());
-			testCase[1][i] = Integer.parseInt(st.nextToken());
+		while(T --> 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
+			bw.write(Integer.toString(n1 + n2) + "\n");
 		}
-		
-		for(int i = 0; i < t; i++) {
-			System.out.printf("%d\n", testCase[0][i] + testCase[1][i]);
-		}
+		bw.flush();
+		br.close();
+		bw.close();
 	}
 
 }
