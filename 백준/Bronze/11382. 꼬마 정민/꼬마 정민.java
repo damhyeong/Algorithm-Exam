4c1ad8422bb8c2de320e50ef908c287a751e9c17
damhyeong <rhdwhdals8@naver.com>
2024-04-05 04:51:23 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -3,6 +3,8 @@ import java.util.*;
 import java.io.*;
 
 /**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	512 MB	186327	74992	68298	40.856%
 문제
 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
 
@@ -17,6 +19,7 @@ A+B+C의 값을 출력한다.
 예제 출력 1 
 7931
  */
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -24,10 +27,11 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		long a = Long.parseLong(st.nextToken());
-		long b = Long.parseLong(st.nextToken());
-		long c = Long.parseLong(st.nextToken());
-		System.out.printf("%d", (a + b + c));
+		Long A = Long.parseLong((String) st.nextElement());
+		Long B = Long.parseLong((String) st.nextElement());
+		Long C = Long.parseLong((String) st.nextElement());
+		
+		System.out.println((A + B + C));
 	}
 
 }
