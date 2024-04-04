6f3a87ca0614b15485ccd0a1ceadf627ad5237b0
damhyeong <rhdwhdals8@naver.com>
2024-04-04 23:38:47 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,8 +1,10 @@
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 /**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	531526	240632	203067	45.403%
 문제
 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
 
@@ -11,6 +13,15 @@ import java.io.*;
 
 출력
 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+예제 입력 1 
+7 3
+예제 출력 1 
+10
+4
+21
+2
+1
  */
 
 public class Main {
@@ -20,10 +31,20 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+		Integer A = Integer.parseInt(st.nextToken());
+		Integer B = Integer.parseInt((String) st.nextElement());
 		
-		System.out.printf("%d\n%d\n%d\n%d\n%d", a + b, a - b, a * b, a / b, a % b);
+		StringBuilder sb = new StringBuilder();
+		sb.append(A + B);
+		sb.append("\n");
+		sb.append(A - B);
+		sb.append("\n");
+		sb.append(A * B);
+		sb.append("\n");
+		sb.append(A / B);
+		sb.append("\n");
+		sb.append(A % B);
+		
+		System.out.println(sb);
 	}
-
 }
