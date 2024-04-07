9180a17281583285154e2fd2b34145c80cd894bb
damhyeong <rhdwhdals8@naver.com>
2024-04-08 00:15:43 +0900

    [Bronze V] Title: A+B - 4, Time: 144 ms, Memory: 15868 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,15 +1,20 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
+A+B - 4
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	392472	141600	118652	35.955%
+
 문제
 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
 입력
 입력은 여러 개의 테스트 케이스로 이루어져 있다.
 
-각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+각 테스트 케이스는 한 줄로 이루어져 있으며, 
+각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
 출력
 각 테스트 케이스마다 A+B를 출력한다.
@@ -20,7 +25,6 @@ import java.io.*;
 3 4
 9 8
 5 2
-
 예제 출력 1 
 2
 5
@@ -34,17 +38,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		String s;
 		
-		while((s = br.readLine()) != null) {
-			st = new StringTokenizer(s);
-			
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			System.out.printf("%d\n", (a + b));
+		String str  = null;
+		StringBuilder sb = new StringBuilder();
+		while((str = br.readLine()) != null) {
+			StringTokenizer st = new StringTokenizer(str);
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			sb.append((A + B) + "\n");
 		}
-		br.close();
+		
+		System.out.println(sb.toString());
 	}
 
 }
