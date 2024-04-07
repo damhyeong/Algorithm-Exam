e922f8a47fbc8509831ce8683c70c899a521bc5f
damhyeong <rhdwhdals8@naver.com>
2024-04-07 23:37:08 +0900

    [Bronze V] Title: A+B - 7, Time: 148 ms, Memory: 15868 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,8 +1,10 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
+A+B - 7 성공
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	290836	143042	123314	49.067%
 문제
 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
@@ -12,7 +14,8 @@ import java.io.*;
 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
 출력
-각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 
+테스트 케이스 번호는 1부터 시작한다.
 
 예제 입력 1 
 5
@@ -21,7 +24,6 @@ import java.io.*;
 3 4
 9 8
 5 2
-
 예제 출력 1 
 Case #1: 2
 Case #2: 5
@@ -35,17 +37,18 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int t = Integer.parseInt(st.nextToken());
-		int num;
-		StringBuilder sb = new StringBuilder();
+		int loop = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < t; i++) {
-			st = new StringTokenizer(br.readLine());
-			num = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
-			sb.append("Case #" + (i + 1) + ": " + num + '\n');
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= loop; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			sb.append("Case #" + i); sb.append(": "); sb.append(A + B); sb.append("\n");
 		}
-		System.out.print(sb);
+		
+		System.out.println(sb.toString());
 	}
 
 }
