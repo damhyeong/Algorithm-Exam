738cc8f8a6a93dd3f8e22c53e88a18e8675994f2
damhyeong <rhdwhdals8@naver.com>
2024-04-16 23:06:53 +0900

    [Bronze I] Title: 진법 변환 2, Time: 164 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,17 +1,24 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
-문제
-10진법 수 N이 주어진다. 이 수를 B진법으로 바꿔 출력하는 프로그램을 작성하시오.
+진법 변환 2
 
-10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다. 이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+0.5 초 (추가 시간 없음)	256 MB	49087	22861	19721	46.585%
+
+문제
+10진법 수 N이 주어진다. 
+이 수를 B진법으로 바꿔 출력하는 프로그램을 작성하시오.
+
+10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다. 
+이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
 
 A: 10, B: 11, ..., F: 15, ..., Y: 34, Z: 35
 
 입력
-첫째 줄에 N과 B가 주어진다. (2 ≤ B ≤ 36) N은 10억보다 작거나 같은 자연수이다.
+첫째 줄에 N과 B가 주어진다. 
+(2 ≤ B ≤ 36) N은 10억보다 작거나 같은 자연수이다.
 
 출력
 첫째 줄에 10진법 수 N을 B진법으로 출력한다.
@@ -28,27 +35,21 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		int decimalNum = Integer.parseInt(st.nextToken());
+		int notation = Integer.parseInt(st.nextToken());
 		
-		int tenNum = Integer.parseInt(st.nextToken());
-		int checkNum = tenNum;
-		int b = Integer.parseInt(st.nextToken());
-		
-		String output = "";
-		
-		while(tenNum != 0) {
-			int intCh = tenNum % b;
-			if(0 <= intCh && intCh <= 9) {
-				output = intCh + output;
-			} else {
-				output = (char)(intCh - 10 + 'A') + output;
-			}
-			
-			tenNum /= b;
+		int mulNotation = 1; // initialize divide Number.
+		String transformNum = ""; // contain result.
+		while(decimalNum != 0) {
+			int tempNum = decimalNum % notation;
+			transformNum = returnIntToChar(tempNum) + transformNum;
+			decimalNum /= notation;
 		}
-		if(checkNum == 0)
-			System.out.println(0);
-		else
-			System.out.println(output);
+		
+		System.out.println(transformNum);
 	}
-
+	
+	public static char returnIntToChar(int num) {
+		return (num < 10) ? (char)('0' + num) : (char)(num - 10 + 'A');
+	}
 }
