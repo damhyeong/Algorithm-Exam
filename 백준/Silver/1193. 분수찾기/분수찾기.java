0995d550180392f32d3b26bed43148e9b3602426
damhyeong <rhdwhdals8@naver.com>
2024-04-17 00:54:47 +0900

    [Silver V] Title: 분수찾기, Time: 148 ms, Memory: 16008 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,8 +1,33 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
+분수찾기
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+0.5 초 (추가 시간 없음)	256 MB	123435	62634	53803	51.603%
+
+문제
+무한히 큰 배열에 다음과 같이 분수들이 적혀있다.
+
+1/1	1/2	1/3	1/4	1/5	…
+2/1	2/2	2/3	2/4	…	…
+3/1	3/2	3/3	…	…	…
+4/1	4/2	…	…	…	…
+5/1	…	…	…	…	…
+…	…	…	…	…	…
+
+이와 같이 나열된 분수들을 1/1 → 1/2 → 2/1 → 3/1 → 2/2 → … 과 같은 지그재그 순서로 
+차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
+
+X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 X(1 ≤ X ≤ 10,000,000)가 주어진다.
+
+출력
+첫째 줄에 분수를 출력한다.
+
 예제 입력 1 
 1
 예제 출력 1 
@@ -43,26 +68,6 @@ import java.io.*;
 14
 예제 출력 10 
 2/4
-
-문제
-무한히 큰 배열에 다음과 같이 분수들이 적혀있다.
-
-1/1	1/2	1/3	1/4	1/5	…
-2/1	2/2	2/3	2/4	…	…
-3/1	3/2	3/3	…	…	…
-4/1	4/2	…	…	…	…
-5/1	…	…	…	…	…
-…	…	…	…	…	…
-이와 같이 나열된 분수들을 1/1 → 1/2 → 2/1 → 3/1 → 2/2 → … 과 같은 지그재그 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
-
-X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 X(1 ≤ X ≤ 10,000,000)가 주어진다.
-
-출력
-첫째 줄에 분수를 출력한다.
-
  */
 
 public class Main {
@@ -70,26 +75,40 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
 		
-		int index = 1;
+		int X = Integer.parseInt(br.readLine());
+		int x = -1, y = -1;
 		
-		int total = 2;
-		
-		while(index < n) {
-			index += total;
-			total++;
+		int count = 0; // 몇 겹 인지. 1겹 = count:2,  2겹 = count:3,  ... 
+		while(X > 0) {
+			X -= count++;
+			
 		}
 		
-		int minusIndex = index - n + 1;
+		X *= -1; // X는 이동해야 하는 횟수를 의미하게 된다. 
 		
-		String str;
-		if(total % 2 == 1) {
-			str = (total - minusIndex) + "/" + (minusIndex);
-		} else {
-			str = (minusIndex) + "/" + (total - minusIndex);
+		if(count % 2 != 0) { 
+			int mvCnt = 0;
+			for(int i = count - 1, j = 1; i >= 1 && j <= count - 1; i--, j++) {
+				if(mvCnt == X) {
+					x = i; y = j;
+					break;
+				}
+				mvCnt++;
+			}
+		} else { 
+			int mvCnt = 0;
+			for(int i = 1, j = count - 1; i <= count - 1 && j >= 1; i++, j--) {
+				if(mvCnt == X) {
+					x = i; y = j;
+					break;
+				}
+				mvCnt++;
+			}
 		}
-		System.out.println(str);
+		
+		System.out.println(x + "/" + y);
+		
 	}
 
 }
