198e59d555eb736bfafeb87e4f90c89e08d47569
damhyeong <rhdwhdals8@naver.com>
2024-04-19 22:13:10 +0900

    [Bronze I] Title: 소인수분해, Time: 168 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,8 +1,22 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
+소인수분해
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	112169	61479	47681	53.386%
+
+문제
+정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
+
+출력
+N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. 
+N이 1인 경우 아무것도 출력하지 않는다.
+
 예제 입력 1 
 72
 예제 출력 1 
@@ -29,16 +43,6 @@ import java.io.*;
 예제 출력 5 
 97
 103
-
-문제
-정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
-
-출력
-N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
-
  */
 
 public class Main {
@@ -46,26 +50,28 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int originNum = Integer.parseInt(br.readLine());
 		
-		StringBuilder sb = new StringBuilder();
 		
-		if(N != 1) {
-			int divNum = N;
-			while(divNum != 1) {
-				for(int i = 2; i <= N; i++) {
-					if(divNum % i == 0) {
-						divNum /= i;
-						sb.append(i + "\n");
+		if(originNum == 1) { // 소인수분해 하는 수가 1일 경우, 아무것도 출력하지 않는다.
+			
+		} else {
+			StringBuilder sb = new StringBuilder();
+			while(originNum != 1) { 
+				
+				int index = 2;
+				while(index <= originNum) {
+					if(originNum % index == 0) { // 낮은 수로 소인수분해가 된다면,
+						sb.append(index); sb.append("\n");
+						originNum /= index;
 						break;
 					}
+					index++;
 				}
 			}
-			System.out.println(sb);
-		} else {
-			System.out.print("");
-		}
 			
+			System.out.println(sb.toString());
+		}
 	}
 
 }
