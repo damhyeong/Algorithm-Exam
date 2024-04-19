9a823c7174e1d49b030a0e5af798c3ce0dfc3d14
damhyeong <rhdwhdals8@naver.com>
2024-04-19 22:00:46 +0900

    [Bronze II] Title: 소수, Time: 140 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,12 +1,17 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
+소수
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	148731	59026	49661	39.416%
+
 문제
 자연수 M과 N이 주어질 때 M이상 N이하의 자연수 중 소수인 것을 모두 골라 이들 소수의 합과 최솟값을 찾는 프로그램을 작성하시오.
 
-예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 소수는 61, 67, 71, 73, 79, 83, 89, 97 총 8개가 있으므로, 이들 소수의 합은 620이고, 최솟값은 61이 된다.
+예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 소수는 61, 67, 71, 73, 79, 83, 89, 97 총 8개가 있으므로, 
+이들 소수의 합은 620이고, 최솟값은 61이 된다.
 
 입력
 입력의 첫째 줄에 M이, 둘째 줄에 N이 주어진다.
@@ -36,39 +41,46 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int M = Integer.parseInt(br.readLine());
 		int N = Integer.parseInt(br.readLine());
 		
-		int sum = 0;
-		int minNum = -1;
+		long sum = 0;
+		int min = Integer.MAX_VALUE;
 		
 		for(int i = M; i <= N; i++) {
-			if(i == 1)
+			int num = i; // 소수인지 아닌지 판별할 수.
+			
+			// 예외 수 - 1, 2 
+			if(num == 1) { // 1은 소수가 아니다. 
 				continue;
+			} else if(num == 2) { // 2는 소수이다. 
+				sum += 2;
+				min = (min >= num) ? num : min;
+				continue;
+			}
 			
-			int num = i;
-			boolean isPrime = true;
-			
-			for(int j = 2; j < num; j++) {
-				if(i % j == 0) {
-					isPrime = false;
+			// 본격적인 소수 판별 
+			boolean isPrimeNum = true;
+			for(int j = 2; j <= (num / 2) + 1; j++) {
+				if(num % j == 0) { // num이 소수가 아니라면 
+					isPrimeNum = false;
 					break;
 				}
 			}
 			
-			if (isPrime) {
-				if(sum == 0)
-					minNum = i;
-				sum += i;
-			} 
+			// 소수가 맞다면 
+			if(isPrimeNum) {
+				min = (min >= num) ? num : min;
+				sum += num;
+			}
 		}
-		StringBuilder sb = new StringBuilder();
-		if(sum == 0) {
+		
+		if(min == Integer.MAX_VALUE)
 			System.out.println("-1");
-		} else {
-			sb.append(sum + "\n");
-			sb.append(minNum);
-			System.out.println(sb);
+		else {
+			System.out.println(sum);
+			System.out.println(min);
 		}
 	}
 
