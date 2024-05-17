c2c900f6c2705c9eb39bf3f4a91081e69f10133d
damhyeong <rhdwhdals8@naver.com>
2024-05-17 10:28:05 +0900

    [Silver IV] Title: 듣보잡, Time: 328 ms, Memory: 26456 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,15 +1,24 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
+듣보잡
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	256 MB	109516	47186	36744	41.313%
 문제
-김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
+김진영이 듣도 못한 사람의 명단과, 
+보도 못한 사람의 명단이 주어질 때, 
+
+듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
 
 입력
 첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다. 
+
 이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다. 
-이름은 띄어쓰기 없이 알파벳 소문자로만 이루어지며, 그 길이는 20 이하이다. N, M은 500,000 이하의 자연수이다.
+
+이름은 띄어쓰기 없이 알파벳 소문자로만 이루어지며, 그 길이는 20 이하이다. 
+
+N, M은 500,000 이하의 자연수이다.
 
 듣도 못한 사람의 명단에는 중복되는 이름이 없으며, 보도 못한 사람의 명단도 마찬가지이다.
 
@@ -36,45 +45,38 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken()); int M = Integer.parseInt(st.nextToken());
 		
-		HashSet<String> noHeard = new HashSet<String>();
-		HashSet<String> noHeardSaw = new HashSet<String>();
+		Set<String> inputSet = new HashSet<>();
+		Set<String> resultSet = new HashSet<>();
 		
-		while(N --> 0) {
-			noHeard.add(br.readLine());
-		}
-		while(M --> 0) {
-			String newName = br.readLine();
-			if(noHeard.contains(newName)) {
-				noHeardSaw.add(newName);
+		for(int i = 0; i < N; i++)
+			inputSet.add(br.readLine().trim());
+		
+		int count = 0;
+		for(int i = 0; i < M; i++) {
+			String findSomeone = br.readLine().trim();
+			
+			if(inputSet.contains(findSomeone)) {
+				resultSet.add(findSomeone);
+				count++;
 			}
 		}
 		
-		LinkedList<String> strArr = new LinkedList<>();
+		String[] strArr = resultSet.toArray(new String[0]);
 		
-		Iterator iter = noHeardSaw.iterator();
+		StringBuilder sb = new StringBuilder();
 		
-		while(iter.hasNext()) {
-			strArr.add((String)iter.next());
-		}
+		Arrays.sort(strArr);
 		
-		strArr.sort(new Comparator<String>() {
-			public int compare(String s1, String s2) {
-				return s1.compareTo(s2);
-			}
-		});
+		sb.append(count); sb.append("\n");
 		
-		bw.write(Integer.toString(strArr.size()) + "\n");
-		Iterator strIter = strArr.iterator();
-		while(strIter.hasNext()) {
-			bw.write(strIter.next() + "\n");
-		}
-		bw.flush();
-		bw.close();
-		br.close();
+		for(int i = 0; i < strArr.length; i++)
+			sb.append(strArr[i] + "\n");
+		
+		System.out.println(sb.toString());
 	}
 
 }
