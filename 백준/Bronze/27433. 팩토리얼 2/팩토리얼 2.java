89137e2f67fa6006f0684682cd41c3fc72715089
damhyeong <rhdwhdals8@naver.com>
2024-05-27 10:25:43 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,19 +1,24 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
 팩토리얼 2
+
 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-1 초	1024 MB	11386	5465	5001	49.940%
+1 초	1024 MB	28186	13155	11794	47.968%
 
 문제
-0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+---
+0보다 크거나 같은 정수 N이 주어진다. 
+
+이때, N!을 출력하는 프로그램을 작성하시오.
 
 입력
+---
 첫째 줄에 정수 N(0 ≤ N ≤ 20)이 주어진다.
 
 출력
+---
 첫째 줄에 N!을 출력한다.
 
 예제 입력 1 
@@ -31,20 +36,16 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		long N = Long.parseLong(br.readLine());
+		long N = Integer.parseInt(br.readLine());
 		
-		long result = factorial(N);
-		bw.write(Long.toString(result));
-		bw.flush();
-		bw.close();
-		br.close();
+		long result = recursive(N);
+		
+		System.out.println(result);
 	}
 	
-	public static long factorial(long num) {
-		if(num <= 1)
+	public static long recursive(long N) {
+		if(N == 0 || N == 1)
 			return 1;
-		else
-			return num * factorial(num - 1);
+		return N * recursive(N - 1);
 	}
 }
