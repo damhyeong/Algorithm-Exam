56f3b8ba74f03613a5dd6df585a71506fdb797d9
damhyeong <rhdwhdals8@naver.com>
2024-06-04 14:56:43 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 148 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,22 +1,26 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
-가장 긴 증가하는 부분 수열
+가장 긴 증가하는 부분 수열 
+
 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-1 초	256 MB	144011	57259	37899	37.722%
+1 초	256 MB	168582	67722	44926	38.074%
+
 문제
+----
 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
 
-예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 가장 긴 증가하는 부분 수열은 {10, 20, 30, 50} 이고, 길이는 4이다.
+예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 가장 긴 증가하는 부분 수열은 A = {10, 20, 10, 30, 20, 50} 이고, 길이는 4이다.
 
 입력
+----
 첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
 
-둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+둘째 줄에는 수열 A를 이루고 있는 A_i가 주어진다. (1 ≤ A_i ≤ 1,000)
 
 출력
+----
 첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
 
 예제 입력 1 
@@ -27,48 +31,36 @@ import java.io.*;
  */
 
 public class Main {
-	public static int[] arr;
-	public static int[] dp;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		int N = Integer.parseInt(br.readLine());
 		
-		arr = new int[N + 1];
-		dp = new int[N + 1];
+		int[] arr = new int[N + 1];
+		int[] dp = new int[N + 1];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= N; i++) {
+		
+		for(int i = 1; i < N + 1; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
+		
+		arr[0] = 0; dp[0] = 0;
+		
+		for(int i = 1; i < N + 1; i++) {
+			int tempResult = Integer.MIN_VALUE;
+			for(int j = 0; j < i; j++)
+				tempResult = (arr[j] < arr[i] && tempResult < dp[j]) ? dp[j] : tempResult;
+			
+			dp[i] = tempResult + 1;
 		}
 		
-		int result = getSequence(N);
-		bw.write(Integer.toString(result));
-		bw.close();
-		br.close();
+		int max = Integer.MIN_VALUE;
+		for(int i = 0; i <= N; i++)
+			max = max < dp[i] ? dp[i] : max;
+		
+		System.out.println(max);
 	}
-	public static int getSequence(int N) {
-		
-		for(int i = 1; i <= N; i++) {
-			dp[i] = 1;
-		    
-			// 0 ~ i 이전 원소들 탐색
-			for(int j = 1; j < i; j++) {
-		    
-				// j번째 원소가 i번째 원소보다 작으면서 i번째 dp가 j번째 dp+1 값보다 작은경우
-				if(arr[j] < arr[i] && dp[i] < dp[j] + 1) {
-					dp[i] = dp[j] + 1;	// j번째 원소의 +1 값이 i번째 dp가 된다.
-				}
-			}
-		}
-		
-		// 최댓값(최대 길이) 탐색 
-		int max = -1;
-		for(int i = 1; i <= N; i++) {
-			max = dp[i] > max ? dp[i] : max;
-		}
-		return max;
-	}
+
 }
