e444f417cda194c7680e4f06b72741006918ec90
damhyeong <rhdwhdals8@naver.com>
2024-06-10 12:50:05 +0900

    [Silver III] Title: 수열, Time: 272 ms, Memory: 23840 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,13 +1,14 @@
-
 import java.util.*;
 import java.io.*;
 
 /**
 수열
+
 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-1 초	128 MB	44944	16437	12968	35.743%
+1 초	128 MB	59247	21970	17036	36.010%
 
 문제
+---
 매일 아침 9시에 학교에서 측정한 온도가 어떤 정수의 수열로 주어졌을 때, 연속적인 며칠 동안의 온도의 합이 가장 큰 값을 알아보고자 한다.
 
 예를 들어, 아래와 같이 10일 간의 온도가 주어졌을 때,
@@ -16,28 +17,31 @@ import java.io.*;
 
 모든 연속적인 이틀간의 온도의 합은 아래와 같다.
 
+1 -6 -13 -9 3 10 20 21 5
 
 
 이때, 온도의 합이 가장 큰 값은 21이다.
 
 또 다른 예로 위와 같은 온도가 주어졌을 때, 모든 연속적인 5일 간의 온도의 합은 아래와 같으며,
 
-
+-12 -12 -3 14 31 28
 
 이때, 온도의 합이 가장 큰 값은 31이다.
 
 매일 측정한 온도가 정수의 수열로 주어졌을 때, 연속적인 며칠 동안의 온도의 합이 가장 큰 값을 계산하는 프로그램을 작성하시오.
 
 입력
+----
 첫째 줄에는 두 개의 정수 N과 K가 한 개의 공백을 사이에 두고 순서대로 주어진다. 
-첫 번째 정수 N은 온도를 측정한 전체 날짜의 수이다. 
-N은 2 이상 100,000 이하이다. 
-두 번째 정수 K는 합을 구하기 위한 연속적인 날짜의 수이다. 
-K는 1과 N 사이의 정수이다. 
-둘째 줄에는 매일 측정한 온도를 나타내는 N개의 정수가 빈칸을 사이에 두고 주어진다. 
-이 수들은 모두 -100 이상 100 이하이다.
+
+첫 번째 정수 N은 온도를 측정한 전체 날짜의 수이다. N은 2 이상 100,000 이하이다. 
+
+두 번째 정수 K는 합을 구하기 위한 연속적인 날짜의 수이다. K는 1과 N 사이의 정수이다. 
+
+둘째 줄에는 매일 측정한 온도를 나타내는 N개의 정수가 빈칸을 사이에 두고 주어진다. 이 수들은 모두 -100 이상 100 이하이다.
 
 출력
+----
 첫째 줄에는 입력되는 온도의 수열에서 연속적인 K일의 온도의 합이 최대가 되는 값을 출력한다.
 
 예제 입력 1 
@@ -45,6 +49,7 @@ K는 1과 N 사이의 정수이다.
 3 -2 -4 -9 0 3 7 13 8 -3
 예제 출력 1 
 21
+
 예제 입력 2 
 10 5
 3 -2 -4 -9 0 3 7 13 8 -3
@@ -57,34 +62,24 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); int K = Integer.parseInt(st.nextToken());
 		
-		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N + 1];
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
 		
-		int maxValue = 0;
-		int cnt = 0;
-		for(int i = 0; i < K; i++) {
-			maxValue += arr[i];
-		}
+		for(int i = 1; i <= N; i++)
+			arr[i] = Integer.parseInt(st.nextToken()) + arr[i - 1];
 		
-		int newValue = maxValue;
-		for(int i = K; i < arr.length; i++) {
-			newValue += arr[i];
-			newValue -= arr[i - K];
-			
-			maxValue = maxValue >= newValue ? maxValue : newValue;
+		int max = Integer.MIN_VALUE;
+		for(int i = K; i <= N; i++) {
+			int sum = arr[i] - arr[i - K];
+			max = max < sum ? sum : max;
 		}
-		bw.write(Integer.toString(maxValue));
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(max);
 	}
 
 }
