bd0c0e62308f4d9c9bafc259d0de06dd6eabec0f
damhyeong <rhdwhdals8@naver.com>
2024-07-05 22:55:11 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n1 = Integer.parseInt(br.readLine());
+        int n2 = Integer.parseInt(br.readLine());
+        
+        
+        System.out.println(n1 + n2);
+    }
+}
