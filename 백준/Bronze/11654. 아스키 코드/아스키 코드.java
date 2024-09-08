f6ab1735ed9fd8bc01c558caa3fe5494bdcfd87b
damhyeong <rhdwhdals8@naver.com>
2024-09-08 23:57:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 96 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,52 +1,16 @@
-
 import java.util.*;
 import java.io.*;
 
-/**
-
-예제 입력 1 
-A
-예제 출력 1 
-65
-예제 입력 2 
-C
-예제 출력 2 
-67
-예제 입력 3 
-0
-예제 출력 3 
-48
-예제 입력 4 
-9
-예제 출력 4 
-57
-예제 입력 5 
-a
-예제 출력 5 
-97
-예제 입력 6 
-z
-예제 출력 6 
-122
-
-문제
-알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
-
-입력
-알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
-
-출력
-입력으로 주어진 글자의 아스키 코드 값을 출력한다.
- */
-
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		char ch = br.readLine().charAt(0);
-		
-		System.out.print((int)ch);
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String inputData = br.readLine();
+        
+        char character = inputData.charAt(0);
+        
+        int ASCII_DATA = (int)character;
+        
+        System.out.println(ASCII_DATA);
+    }
 }
