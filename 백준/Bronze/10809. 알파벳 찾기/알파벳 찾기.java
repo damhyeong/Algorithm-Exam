8a07238195e6e9725f5538944919c79443a00098
damhyeong <rhdwhdals8@naver.com>
2024-09-10 19:59:46 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,53 +1,34 @@
 import java.util.*;
 import java.io.*;
 
-/**
-알파벳 찾기
-
-시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-1 초	256 MB	221440	118578	97642	53.285%
-
-문제
-알파벳 소문자로만 이루어진 단어 S가 주어진다. 
-각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 단어 S가 주어진다. 단어의 길이는 100을 넘지 않으며, 알파벳 소문자로만 이루어져 있다.
-
-출력
-각각의 알파벳에 대해서, a가 처음 등장하는 위치, b가 처음 등장하는 위치, ... z가 처음 등장하는 위치를 공백으로 구분해서 출력한다.
-
-만약, 어떤 알파벳이 단어에 포함되어 있지 않다면 -1을 출력한다. 단어의 첫 번째 글자는 0번째 위치이고, 두 번째 글자는 1번째 위치이다.
-
-예제 입력 1 
-baekjoon
-예제 출력 1 
-1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
- */
-
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] numsChar = new int['z' - 'a' + 1];
-		
-		Arrays.fill(numsChar, -1);
-		
-		String str = br.readLine().trim();
-		
-		for(int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-			
-			numsChar[ch - 'a'] = (numsChar[ch - 'a'] == -1) ? i : numsChar[ch - 'a'];
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < numsChar.length; i++) {
-			sb.append(numsChar[i] + " ");
-		}
-		
-		System.out.println(sb.toString());
-	}
-
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+       // 문자들의 첫 번째 수를 담을 배열 생성 
+       int[] arr = new int['z' - 'a' + 1];
+       
+       // 반복문 말고도 이러한 방식을 사용 할 수 있다.
+       // 단순 배열에 대한 편의성 객체를 이용하여 내부의 값을 채움.
+       Arrays.fill(arr, -1);
+       
+       String str = br.readLine();
+       
+       for(int i = 0; i < str.length(); i++) {
+          char ch = str.charAt(i);
+          
+          /**
+           만약 현재 문자 순서가 삽입되었다면, 값을 변경하지 않으며,
+           현재 문자의 순서가 -1 로 삽입 된 적이 없다면, 현재의 인덱스로 변경한다.
+           */
+          arr[ch - 'a'] = arr[ch - 'a'] == -1 ? i : arr[ch - 'a'];
+       }
+       
+       // System.out.print() 사용해도 되지만, StringBuilder 로 더 깔끔하게 만들 수 있다.
+       StringBuilder sb = new StringBuilder();
+       for(int i = 0; i < arr.length; i++)
+          sb.append(arr[i]).append(" ");
+       
+       System.out.println(sb.toString());
+    }
 }
