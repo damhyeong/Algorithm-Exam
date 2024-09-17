72976379f82de1578556f16823da977ae97e722c
damhyeong <rhdwhdals8@naver.com>
2024-09-18 02:05:14 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 112 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,57 +1,30 @@
-
 import java.util.*;
 import java.io.*;
 
-/**
-문제
-예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
-
-입력
-첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
-
-출력
-첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
-
-예제 입력 1 
-5
-예제 출력 1 
-    *
-   ***
-  *****
- *******
-*********
- *******
-  *****
-   ***
-    *
- */
-
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0, j = 2 * (n - 1); i < (2 * n - 1); i++, j--) {
-			if(i <= j) {
-				for(int x = 0; x < n - (i + 1); x++)
-					sb.append(" ");
-				for(int x = 0; x < 2 * i + 1; x++) {
-					sb.append("*");
-				}
-				sb.append("\n");
-			} else {
-				for(int x = 0; x < n - (j + 1); x++)
-					sb.append(" ");
-				for(int x = 0; x < 2 * j + 1; x++) {
-					sb.append("*");
-				}
-				sb.append("\n");
-			}
-		}
-		System.out.print(sb);
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < 2 * N; i++) {
+            int blank = abs(-1 * i + N);
+            int star = -1 * abs(2 * (i - N)) + (2 * N - 1);
+            for(int j = 0; j < blank; j++)
+                sb.append(' ');
+            for(int j = 0; j < star; j++)
+                sb.append('*');
+            
+            sb.append("\n");
+        }
+        
+        System.out.println(sb.toString());
+    }
+    public static int abs(int num){
+        if(num < 0)
+            return -1 * num;
+        else
+            return num;
+    }
 }
