f2661c7396cca09b20ff8a0e6a1aa428521cd13a
damhyeong <rhdwhdals8@naver.com>
2024-10-30 04:56:00 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,62 +1,41 @@
-
 import java.util.*;
 import java.io.*;
 
-/**
-가로, 세로의 크기가 각각 100인 정사각형 모양의 흰색 도화지가 있다. 
-이 도화지 위에 가로, 세로의 크기가 각각 10인 정사각형 모양의 검은색 색종이를 색종이의 변과 도화지의 변이 평행하도록 붙인다. 
-이러한 방식으로 색종이를 한 장 또는 여러 장 붙인 후 색종이가 붙은 검은 영역의 넓이를 구하는 프로그램을 작성하시오.
-
-
-예를 들어 흰색 도화지 위에 세 장의 검은색 색종이를 그림과 같은 모양으로 붙였다면 검은색 영역의 넓이는 260이 된다.
-
-입력
-첫째 줄에 색종이의 수가 주어진다. 이어 둘째 줄부터 한 줄에 하나씩 색종이를 붙인 위치가 주어진다. 
-색종이를 붙인 위치는 두 개의 자연수로 주어지는데 
-첫 번째 자연수는 색종이의 왼쪽 변과 도화지의 왼쪽 변 사이의 거리이고, 
-두 번째 자연수는 색종이의 아래쪽 변과 도화지의 아래쪽 변 사이의 거리이다. 
-색종이의 수는 100 이하이며, 색종이가 도화지 밖으로 나가는 경우는 없다
-
-출력
-첫째 줄에 색종이가 붙은 검은 영역의 넓이를 출력한다.
-
-예제 입력 1 
-3
-3 7
-15 7
-5 2
-예제 출력 1 
-260
- */
-
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int[][] matrix = new int[100][100];
-		
-		int t = Integer.parseInt(st.nextToken());
-		
-		int cnt = 0;
-		
-		for(int i = 0; i < t; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken()) - 1;
-			int y = Integer.parseInt(st.nextToken()) - 1;
-			
-			for(int j = x; j < x + 10; j++) {
-				for(int k = y; k < y + 10; k++) {
-					if(matrix[j][k] != 1) {
-						matrix[j][k] = 1;
-						cnt++;
-					}
-				}
-			}
-		}
-		System.out.print(cnt);
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int loop = Integer.parseInt(br.readLine());
+        
+        // 정해진 도화지의 넓이는 100 * 100
+        boolean[][] areas = new boolean[100][100];
+        
+        /*
+         1 번째 - loop = 3
+         2 번째 - loop = 2
+         3 번째 - loop = 1
+         4 번째 - loop = 0 XXXX
+         즉, 후위 연산자를 통해 나중에 계산하는 것.
+         */
+        while(loop-- > 0) { // while(--loop >= 0) 도 동일한 로직.
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            for(int i = x; i < x + 10; i++) {
+                for (int j = y; j < y + 10; j++) {
+                    areas[i][j] = true;
+                }
+            }
+        }
+        
+        int black = 0;
+        for(int i = 0; i < areas.length; i++) {
+            for (int j = 0; j < areas[0].length; j++) {
+                if (areas[i][j])
+                    black++;
+            }
+        }
+        System.out.println(black);
+    }
 }
