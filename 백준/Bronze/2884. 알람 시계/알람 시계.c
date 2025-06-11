da65abc748f7aca3cc1142782db5bc96a548f7ce
damhyeong <rhdwhdals8@naver.com>
2025-06-11 22:45:56 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,231 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+*/
+
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	571079	218092	178718	37.984%
+
+문제
+---
+상근이는 매일 아침 알람을 듣고 일어난다.
+
+알람을 듣고 바로 일어나면 다행이겠지만, 항상 조금만 더 자려는 마음 때문에 매일 학교를 지각하고 있다.
+
+상근이는 모든 방법을 동원해보았지만, 조금만 더 자려는 마음은 그 어떤 것도 없앨 수가 없었다.
+
+이런 상근이를 불쌍하게 보던 창영이는 자신이 사용하는 방법을 추천해 주었다.
+
+바로 "45분 일찍 알람 설정하기"이다.
+
+이 방법은 단순하다. 원래 설정되어 있는 알람을 45분 앞서는 시간으로 바꾸는 것이다.
+
+어차피 알람 소리를 들으면, 알람을 끄고 조금 더 잘 것이기 때문이다.
+
+이 방법을 사용하면, 매일 아침 더 잤다는 기분을 느낄 수 있고, 학교도 지각하지 않게 된다.
+
+현재 상근이가 설정한 알람 시각이 주어졌을 때, 창영이의 방법을 사용한다면, 이를 언제로 고쳐야 하는지 구하는 프로그램을 작성하시오.
+
+입력
+---
+첫째 줄에 두 정수 H와 M이 주어진다. (0 ≤ H ≤ 23, 0 ≤ M ≤ 59) 그리고 이것은 현재 상근이가 설정한 알람 시간 H시 M분을 의미한다.
+
+입력 시간은 24시간 표현을 사용한다. 24시간 표현에서 하루의 시작은 0:0(자정)이고, 끝은 23:59(다음날 자정 1분 전)이다. 시간을 나타낼 때, 불필요한 0은 사용하지 않는다.
+
+출력
+---
+첫째 줄에 상근이가 창영이의 방법을 사용할 때, 설정해야 하는 알람 시간을 출력한다. (입력과 같은 형태로 출력하면 된다.)
+
+예제 입력 1
+10 10
+예제 출력 1
+9 25
+예제 입력 2
+0 30
+예제 출력 2
+23 45
+예제 입력 3
+23 40
+예제 출력 3
+22 55
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t);
+extern void free(void*);
+extern void* realloc(void*, size_t);
+
+char** tokenizer(const char* input);
+void freeTokens(char** tokens);
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+int main () {
+    char input[255];
+
+    fgets(input, sizeof(input), stdin);
+
+    char** tokens = (char**) tokenizer(input);
+    char** tokensPtr = (char**)tokens;
+
+
+    int hour = parseInt(*tokensPtr++);
+    int min = parseInt(*tokensPtr);
+
+    freeTokens(tokens);
+
+    min -= 45;
+
+    if(min < 0) {
+        min = 60 + min;
+        hour--;
+    }
+
+    if(hour < 0){
+        hour = 24 + hour;
+    }
+
+    char* hourStr = intToStr(hour);
+    char* minStr = intToStr(min);
+
+    fputs(hourStr, stdout);
+    fputc(' ', stdout);
+    fputs(minStr, stdout);
+    fputc('\n', stdout);
+
+    free(hourStr); free(minStr);
+
+    return 0;
+}
+
+char** tokenizer(const char* input) {
+    int size = 5;
+    int currSize = 0;
+
+    char** tokens = (char**)malloc(sizeof(char*) * (size + 1));
+    char** tokensPtr = (char**)tokens;
+
+    int tknIdx = 0;
+    int currIdx = 0;
+
+    char* inputPtr = (char*)input;
+
+    while(*inputPtr) {
+        char ch = *inputPtr++;
+
+        if(isBlank(ch) == 1) {
+            if(tknIdx == currIdx) {
+                tknIdx = ++currIdx;
+                continue;
+            }
+
+            int tknSize = currIdx - tknIdx;
+            char* newTkn = (char*)malloc(sizeof(char) * (tknSize + 1));
+            newTkn[tknSize] = '\0';
+            char* newTknPtr = (char*)newTkn;
+
+            while(tknIdx != currIdx) {
+                *newTknPtr++ = input[tknIdx++];
+            }
+
+            tknIdx = ++currIdx;
+
+            *tokensPtr++ = newTkn; currSize++;
+
+            if(currSize == size) {
+                size *= 2;
+                tokens = (char**)realloc(tokens, sizeof(char*) * (size + 1));
+                tokensPtr = (char**)(tokens + currSize);
+            }
+        } else {
+            currIdx++;
+        }
+    }
+
+    *tokensPtr = NULL;
+
+    return tokens;
+}
+
+void freeTokens(char** tokens) {
+    char** freeTknPtr = (char**)tokens;
+
+    while(*freeTknPtr) {
+        free(*freeTknPtr++);
+    }
+    free(tokens);
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        sign = -1;
+        target *= -1;
+    }
+
+    int temp = target;
+
+    do {
+        len++;
+        temp /= 10;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    result[size] = '\0';
+
+    for(int i = endIdx; i >= startIdx; i--) {
+        result[i] = target % 10 + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        result[0] = '-';
+    }
+
+    return result;
+}
+
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(*strPtr) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+
+_Bool isBlank(char ch) {
+    if(ch == 32) {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
