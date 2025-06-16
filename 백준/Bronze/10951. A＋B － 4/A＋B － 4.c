5769d26845a6b5eac11763655e5cd895f56a9b5a
damhyeong <rhdwhdals8@naver.com>
2025-06-16 20:42:06 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.c
@@ -0,0 +1,197 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+*/
+
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	471356	172189	144066	36.274%
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+입력은 여러 개의 테스트 케이스로 이루어져 있다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+각 테스트 케이스마다 A+B를 출력한다.
+
+예제 입력 1
+1 1
+2 3
+3 4
+9 8
+5 2
+예제 출력 1
+2
+5
+7
+17
+7
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t);
+extern void free(void*);
+extern void* realloc(void*, size_t);
+
+
+char** tokenizer(const char* input);
+void freeTokens(char** tokens);
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+
+int main(void) {
+    char input[255];
+
+    // EOF 나 스트림 오류 시 NULL 을 반환한다.
+    while(fgets(input, sizeof(input), stdin) != NULL) {
+        char** tokens = (char**)tokenizer(input);
+        char** tokensPtr = (char**)tokens;
+
+        int n1 = parseInt(*tokensPtr++);
+        int n2 = parseInt(*tokensPtr++);
+
+        freeTokens(tokens);
+
+        int result = n1 + n2;
+        char* resultStr = intToStr(result);
+
+        fputs(resultStr, stdout); fputc('\n', stdout);
+
+        free(resultStr);
+    }
+
+    return 0;
+}
+
+char** tokenizer(const char* input) {
+    int size = 5;
+    int currSize = 0;
+
+    char** tokens = (char**)malloc(sizeof(char*) * (size + 1));
+    char** tokensPtr = (char**)tokens;
+
+    char* inputPtr = (char*)input;
+
+    int tknIdx = 0;
+    int currIdx = 0;
+
+    while(*inputPtr) {
+        char ch = *inputPtr++;
+
+        if(isBlank(ch) == 1) {
+            if(tknIdx == currIdx) {
+                tknIdx = ++currIdx;
+                continue;
+            }
+
+            int tknSize = currIdx - tknIdx;
+            char* newTkn = (char*)malloc(sizeof(char) * (tknSize + 1));
+            newTkn[tknSize] = '\0';
+            char* newTknPtr = (char*)newTkn;
+
+            while(tknIdx != currIdx) {
+                *newTknPtr++ = input[tknIdx++];
+            }
+
+            *tokensPtr++ = newTkn; currSize++;
+
+            tknIdx = ++currIdx;
+
+            if(size == currSize) {
+                size *= 2;
+                tokens = (char**)realloc(tokens, sizeof(char*) * (size + 1));
+                tokensPtr = (char**)(tokens + currSize);
+            }
+        } else {
+            currIdx++;
+        }
+    }
+    *tokensPtr = NULL;
+
+    return tokens;
+}
+
+void freeTokens(char** tokens) {
+    char** tokensPtr = (char**)tokens;
+    while(*tokensPtr) {
+        free(*tokensPtr++);
+    }
+    free(tokens);
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        target *= -1;
+        sign = -1;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    result[size] = '\0';
+
+    for(int i = endIdx; i >= startIdx; i--) {
+        result[i] = target % 10 + '0';
+        target /= 10;
+    }
+
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == '\0') {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
