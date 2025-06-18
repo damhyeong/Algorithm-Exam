ac34d0005fef72499dee21211894d7a1fa6e4e9f
damhyeong <rhdwhdals8@naver.com>
2025-06-18 20:14:09 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.c
@@ -0,0 +1,144 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+*/
+
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	389573	179991	147763	45.492%
+문제
+9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+
+예를 들어, 서로 다른 9개의 자연수
+
+3, 29, 38, 12, 57, 74, 40, 85, 61
+
+이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
+
+입력
+첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
+
+출력
+첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
+
+예제 입력 1
+3
+29
+38
+12
+57
+74
+40
+85
+61
+예제 출력 1
+85
+8
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t);
+extern void free(void*);
+extern void* realloc(void*, size_t);
+
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+int main(void) {
+    char input[100];
+
+    fgets(input, sizeof(input), stdin);
+
+    int biggestNum = parseInt(input);
+    int biggestIdx = 1;
+    int index = 1;
+
+    while(fgets(input, sizeof(input), stdin) != NULL) {
+        index++;
+        int n = parseInt(input);
+
+        if(n >= biggestNum) {
+            biggestNum = n;
+            biggestIdx = index;
+        }
+    }
+
+    char* numStr = intToStr(biggestNum); char* numIdx = intToStr(biggestIdx);
+
+    fputs(numStr, stdout); fputc('\n', stdout);
+    fputs(numIdx, stdout); fputc('\n', stdout);
+
+    free(numStr); free(numIdx);
+
+    return 0;
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        target *= -1;
+        sign = -1;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    result[size] = '\0';
+
+    for(int i = endIdx; i >= startIdx; i--) {
+        result[i] = target % 10 + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == '\0') {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
