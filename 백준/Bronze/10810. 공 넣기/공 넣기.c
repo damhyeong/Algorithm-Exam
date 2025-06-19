3b59a9483c7f65ce627ba936b757f291755edb08
damhyeong <rhdwhdals8@naver.com>
2025-06-19 21:53:10 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.c
@@ -0,0 +1,243 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+*/
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	125495	65112	56607	52.066%
+
+문제
+---
+도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 매겨져 있다.
+
+또, 1번부터 N번까지 번호가 적혀있는 공을 매우 많이 가지고 있다.
+
+가장 처음 바구니에는 공이 들어있지 않으며, 바구니에는 공을 1개만 넣을 수 있다.
+
+도현이는 앞으로 M번 공을 넣으려고 한다.
+
+도현이는 한 번 공을 넣을 때, 공을 넣을 바구니 범위를 정하고, 정한 바구니에 모두 같은 번호가 적혀있는 공을 넣는다.
+
+만약, 바구니에 공이 이미 있는 경우에는 들어있는 공을 빼고, 새로 공을 넣는다.
+
+공을 넣을 바구니는 연속되어 있어야 한다.
+
+
+공을 어떻게 넣을지가 주어졌을 때,
+
+M번 공을 넣은 이후에 각 바구니에 어떤 공이 들어 있는지 구하는 프로그램을 작성하시오.
+
+입력
+---
+첫째 줄에 N (1 ≤ N ≤ 100)과 M (1 ≤ M ≤ 100)이 주어진다.
+
+둘째 줄부터 M개의 줄에 걸쳐서 공을 넣는 방법이 주어진다.
+
+각 방법은 세 정수 i j k로 이루어져 있으며, i번 바구니부터 j번 바구니까지에 k번 번호가 적혀져 있는 공을 넣는다는 뜻이다.
+
+예를 들어, 2 5 6은 2번 바구니부터 5번 바구니까지에 6번 공을 넣는다는 뜻이다.
+
+(1 ≤ i ≤ j ≤ N, 1 ≤ k ≤ N)
+
+도현이는 입력으로 주어진 순서대로 공을 넣는다.
+
+출력
+---
+1번 바구니부터 N번 바구니에 들어있는 공의 번호를 공백으로 구분해 출력한다.
+
+공이 들어있지 않은 바구니는 0을 출력한다.
+
+
+예제 입력 1
+5 4
+1 2 3
+3 4 4
+1 4 1
+2 2 2
+예제 출력 1
+1 2 1 1 0
+*/
+
+
+#include<stdio.h>
+
+extern void* malloc(size_t);
+extern void* calloc(size_t, size_t);
+extern void free(void*);
+extern void* realloc(void*, size_t);
+
+char** tokenizer(const char* input);
+void freeTokens(char** tokens);
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+int main (void) {
+    char input[255];
+
+    fgets(input, sizeof(input), stdin);
+
+    char** tokens = (char**) tokenizer(input);
+    char** tokensPtr = (char**)tokens;
+
+    int N = parseInt(*tokensPtr++);
+    int M = parseInt(*tokensPtr++);
+
+    freeTokens(tokens);
+
+    int* baskets = (int*) calloc(N, sizeof(int));
+
+    while(--M >= 0) {
+        fgets(input, sizeof(input), stdin);
+
+        tokens = tokenizer(input);
+        tokensPtr = (char**)tokens;
+
+        int i = parseInt(*tokensPtr++);
+        int j = parseInt(*tokensPtr++);
+        int k = parseInt(*tokensPtr++);
+
+        freeTokens(tokens);
+
+        while(i <= j) {
+            baskets[i++ - 1] = k;
+        }
+    }
+
+    int* basketsPtr = (int*)baskets;
+
+    int idx = 0;
+    while(idx++ < N) {
+        fputs(intToStr(*basketsPtr++), stdout); fputc(' ', stdout);
+    }
+    fputc('\n', stdout);
+
+    free(baskets);
+
+    return 0;
+}
+
+char** tokenizer(const char* input) {
+    int size = 5;
+    int currSize = 0;
+
+    char** tokens = (char**)malloc(sizeof(char*) * (size + 1));
+    char** tokensPtr = (char**)tokens;
+
+    char* inputPtr = (char*)input;
+
+    int tknIdx = 0;
+    int currIdx = 0;
+
+    while(*inputPtr) {
+        char ch = *inputPtr++;
+
+        if(isBlank(ch) == 1) {
+            if(tknIdx == currIdx) {
+                tknIdx = ++currIdx;
+                continue;
+            }
+
+            int tknSize = currIdx - tknIdx;
+            char* newTkn = (char*)malloc(sizeof(char) * (tknSize + 1));
+            newTkn[tknSize] = '\0';
+            char* newTknPtr = (char*)newTkn;
+
+            while(tknIdx != currIdx) {
+                *newTknPtr++ = input[tknIdx++];
+            }
+
+            *tokensPtr++ = newTkn; currSize++;
+
+            tknIdx = ++currIdx;
+
+            if(size == currSize) {
+                size *= 2;
+                tokens = (char**)realloc(tokens, sizeof(char*) * (size + 1));
+                tokensPtr = (char**)(tokens + currSize);
+            }
+        } else {
+            currIdx++;
+        }
+    }
+    *tokensPtr = NULL;
+
+    return tokens;
+}
+void freeTokens(char** tokens) {
+    char** tokensPtr = (char**)tokens;
+
+    while(*tokensPtr) {
+        free(*tokensPtr++);
+    }
+    free(tokens);
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        target *= -1;
+        sign = -1;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    result[size] = '\0';
+
+    int i = endIdx;
+    while(i >= startIdx) {
+        result[i--] = target % 10 + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == '\0') {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
