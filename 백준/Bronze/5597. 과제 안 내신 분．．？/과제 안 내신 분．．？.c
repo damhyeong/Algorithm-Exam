af93f5249b19dd1c93805f0817fa639adacd6b41
damhyeong <rhdwhdals8@naver.com>
2025-06-20 21:54:13 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.c
@@ -0,0 +1,199 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+*/
+
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	171324	88717	76160	51.585%
+
+문제
+---
+X대학 M교수님은 프로그래밍 수업을 맡고 있다.
+
+교실엔 학생이 30명이 있는데, 학생 명부엔 각 학생별로 1번부터 30번까지 출석번호가 붙어 있다.
+
+교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를 구하는 프로그램을 작성하시오.
+
+입력
+---
+입력은 총 28줄로 각 제출자(학생)의 출석번호 n(1 ≤ n ≤ 30)가 한 줄에 하나씩 주어진다. 출석번호에 중복은 없다.
+
+출력
+---
+출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
+
+예제 입력 1
+3
+1
+4
+5
+7
+9
+6
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+예제 출력 1
+2
+8
+예제 입력 2
+9
+30
+6
+12
+10
+20
+21
+11
+7
+5
+28
+4
+18
+29
+17
+19
+27
+13
+16
+26
+14
+23
+22
+15
+3
+1
+24
+25
+예제 출력 2
+2
+8
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t byte);
+extern void* calloc(size_t size, size_t byte);
+extern void free(void* memory);
+
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+int main (void) {
+    char input[100];
+
+    int size = 30;
+    int* students = (int*)calloc(size + 1, sizeof(int));
+    int* stdPtr = (int*)students;
+
+    while(fgets(input, sizeof(input), stdin) != NULL) {
+        int currNum = parseInt(input);
+
+        *(students + currNum - 1) = 1;
+    }
+
+    int idx = 0;
+    while(idx++ < size) {
+        _Bool isSumbit = *stdPtr++;
+
+        if(isSumbit == 0) {
+            char* tempResult = intToStr(idx);
+
+            fputs(tempResult, stdout); fputc('\n', stdout);
+
+            free(tempResult);
+        }
+    }
+
+    free(students);
+
+    return 0;
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        sign = -1;
+        target *= sign;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while (temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    result[size] = '\0';
+
+    int i = endIdx;
+    while(i >= startIdx) {
+        result[i--] = target % 10 + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == '\0') {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
