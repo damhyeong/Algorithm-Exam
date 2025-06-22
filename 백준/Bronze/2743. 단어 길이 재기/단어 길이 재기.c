695b55c922cce3de9aabb51bd6306cc9845671ca
damhyeong <rhdwhdals8@naver.com>
2025-06-22 22:19:03 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,101 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+*/
+
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	111313	87819	78446	79.842%
+
+문제
+---
+알파벳으로만 이루어진 단어를 입력받아, 그 길이를 출력하는 프로그램을 작성하시오.
+
+입력
+---
+첫째 줄에 영어 소문자와 대문자로만 이루어진 단어가 주어진다. 단어의 길이는 최대 100이다.
+
+출력
+---
+첫째 줄에 입력으로 주어진 단어의 길이를 출력한다.
+
+예제 입력 1
+pulljima
+예제 출력 1
+8
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t byte);
+extern void free(void* memory);
+
+char* intToStr(int target);
+_Bool isBlank(char ch);
+
+int main(void) {
+    char ch;
+
+    int len = 0;
+
+    while(isBlank(ch = fgetc(stdin)) != 1) {
+        len++;
+    }
+
+    char* result = intToStr(len);
+
+    fputs(result, stdout); fputc('\n', stdout);
+
+    return 0;
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        target *= -1;
+        sign = -1;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while (temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    result[size] = '\0';
+
+    int idx = endIdx;
+    while(idx >= startIdx) {
+        result[idx--] = (target % 10) + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == '\0') {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
