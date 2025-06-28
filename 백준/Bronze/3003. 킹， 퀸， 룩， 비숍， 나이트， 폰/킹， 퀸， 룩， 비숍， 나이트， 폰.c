6ae22860f28a62ccb9879d40f2fd76d913d23114
damhyeong <rhdwhdals8@naver.com>
2025-06-29 00:52:33 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -0,0 +1,221 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+
+    참고로, AI 작성 글이 아니라, 진짜 공식문서 뒤져가면서 힘들게 얻은 지식을 나눠보려고 합니다.
+*/
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	148794	87912	77195	59.883%
+
+문제
+---
+동혁이는 오래된 창고를 뒤지다가 낡은 체스판과 피스를 발견했다.
+
+체스판의 먼지를 털어내고 걸레로 닦으니 그럭저럭 쓸만한 체스판이 되었다.
+
+하지만, 검정색 피스는 모두 있었으나, 흰색 피스는 개수가 올바르지 않았다.
+
+체스는 총 16개의 피스를 사용하며,
+
+킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개로 구성되어 있다.
+
+동혁이가 발견한 흰색 피스의 개수가 주어졌을 때,
+
+몇 개를 더하거나 빼야 올바른 세트가 되는지 구하는 프로그램을 작성하시오.
+
+
+입력
+---
+첫째 줄에 동혁이가 찾은 흰색 킹, 퀸, 룩, 비숍, 나이트, 폰의 개수가 주어진다.
+
+이 값은 0보다 크거나 같고 10보다 작거나 같은 정수이다.
+
+
+출력
+---
+첫째 줄에 입력에서 주어진 순서대로 몇 개의 피스를 더하거나 빼야 되는지를 출력한다.
+
+만약 수가 양수라면 동혁이는 그 개수 만큼 피스를 더해야 하는 것이고, 음수라면 제거해야 하는 것이다.
+
+
+예제 입력 1
+0 1 2 2 2 7
+예제 출력 1
+1 0 0 0 0 1
+
+예제 입력 2
+2 1 2 1 2 1
+예제 출력 2
+-1 0 0 1 0 7
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t byte);
+extern void free(void* memory);
+extern void* realloc(void* memory, size_t byte);
+
+int diff(int a, int b);
+
+char** tokenizer(const char* input);
+void freeTokens(char** tokens);
+
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+int main(void) {
+    char input[100];
+
+    int base[] = {1, 1, 2, 2, 2, 8};
+    int* basePtr = (int*)base;
+
+    fgets(input, sizeof(input), stdin);
+
+    char** tokens = (char**)tokenizer(input);
+    char** tokensPtr = (char**)tokens;
+
+    while(*tokensPtr && *basePtr) {
+        int currNum = parseInt(*tokensPtr++);
+
+        int baseNum = *basePtr++;
+
+        char* resultStr = intToStr(baseNum - currNum);
+
+        fputs(resultStr, stdout); fputc(' ', stdout);
+
+        free(resultStr);
+    }
+    fputc('\n', stdout);
+
+    freeTokens(tokens);
+
+    return 0;
+}
+
+
+char** tokenizer(const char* input) {
+    int size = 5;
+    int currSize = 0;
+
+    char** tokens = (char**)malloc(sizeof(char*) * (size + 1));
+    char** tokensPtr = (char**)tokens;
+
+    char* inputPtr = (char*)input;
+
+    int tknIdx = 0;
+    int currIdx = 0;
+
+    while(*inputPtr) {
+        char ch = *inputPtr++;
+
+        if(isBlank(ch) == 1) {
+            if(tknIdx == currIdx) {
+                tknIdx = ++currIdx;
+                continue;
+            }
+
+            int tknSize = currIdx - tknIdx;
+            char* newTkn = (char*)malloc(sizeof(char) * (tknSize + 1));
+            *(newTkn + tknSize) = 0;
+            char* newTknPtr = (char*)newTkn;
+
+            while(tknIdx != currIdx) {
+                *newTknPtr++ = *(input + tknIdx++);
+            }
+
+            *tokensPtr++ = newTkn; currSize++;
+
+            tknIdx = ++currIdx;
+
+            if(size == currSize) {
+                size *= 2;
+                tokens = (char**)realloc(tokens, sizeof(char*) * (size + 1));
+                tokensPtr = (char**)(tokens + currSize);
+            }
+        } else {
+            currIdx++;
+        }
+    }
+    *tokensPtr = NULL;
+
+    return tokens;
+}
+void freeTokens(char** tokens) {
+    char** tokensPtr = (char**)tokens;
+
+    while(*tokensPtr) {
+        free(*tokensPtr++);
+    }
+    free(tokens);
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        target *= -1;
+        sign = -1;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    *(result + size) = 0;
+
+    int idx = endIdx;
+    while(idx >= startIdx) {
+        *(result + idx--) = (target % 10) + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == 0) {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
