2aa393d54b49812c9d2316d6ab612c1b199a8dac
damhyeong <rhdwhdals8@naver.com>
2025-07-06 04:04:51 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.c
@@ -0,0 +1,234 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+
+    참고로, AI 작성 글이 아니라, 진짜 공식문서 뒤져가면서 힘들게 얻은 지식을 나눠보려고 합니다.
+*/
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	92150	46007	40458	50.494%
+
+문제
+---
+어떤 자연수 p와 q가 있을 때,
+
+만일 p를 q로 나누었을 때 나머지가 0이면 q는 p의 약수이다.
+
+6을 예로 들면
+
+6 ÷ 1 = 6 … 0
+6 ÷ 2 = 3 … 0
+6 ÷ 3 = 2 … 0
+6 ÷ 4 = 1 … 2
+6 ÷ 5 = 1 … 1
+6 ÷ 6 = 1 … 0
+
+그래서 6의 약수는 1, 2, 3, 6, 총 네 개이다.
+
+두 개의 자연수 N과 K가 주어졌을 때,
+
+N의 약수들 중 K번째로 작은 수를 출력하는 프로그램을 작성하시오.
+
+
+입력
+---
+첫째 줄에 N과 K가 빈칸을 사이에 두고 주어진다.
+
+N은 1 이상 10,000 이하이다.
+
+K는 1 이상 N 이하이다.
+
+
+출력
+---
+첫째 줄에 N의 약수들 중 K번째로 작은 수를 출력한다.
+
+만일 N의 약수의 개수가 K개보다 적어서 K번째 약수가 존재하지 않을 경우에는 0을 출력하시오.
+
+예제 입력 1
+6 3
+예제 출력 1
+3
+예제 입력 2
+25 4
+예제 출력 2
+0
+예제 입력 3
+2735 1
+예제 출력 3
+1
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t byte);
+extern void free(void* memory);
+extern void* realloc(void* memory, size_t byte);
+extern void* calloc(size_t size, size_t byte);
+
+char** tokenizer(const char* input);
+void freeTokens(char** tokens);
+
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+int main(void) {
+    char input[100];
+
+    fgets(input, sizeof(input), stdin);
+
+    char** tokens = (char**)tokenizer(input);
+    char** tokensPtr = (char**)tokens;
+
+    int N = parseInt(*tokensPtr++);
+    int K = parseInt(*tokensPtr++);
+
+    int div = 1;
+    int currLen = 0;
+
+    int result = 0;
+    while(div <= N) {
+        if(N % div == 0) {
+            currLen++;
+        }
+        if(currLen == K) {
+            result = div;
+            break;
+        }
+
+        ++div;
+    }
+
+    char* resultStr = (char*)intToStr(result);
+
+    fputs(resultStr, stdout); fputc('\n', stdout);
+
+    free(resultStr);
+
+    return 0;
+}
+
+char** tokenizer(const char* input) {
+    int size = 5;
+    int currSize = 0;
+
+    char** tokens = (char**)malloc(sizeof(char*) * (size + 1));
+    char** tokensPtr = (char**)tokens;
+
+    char* inputPtr = (char*)input;
+
+    int tknIdx = 0;
+    int currIdx = 0;
+
+    while(*inputPtr) {
+        char ch = *inputPtr++;
+
+        if(isBlank(ch) == 1) {
+            if(tknIdx == currIdx) {
+                tknIdx = ++currIdx;
+                continue;
+            }
+
+            int tknSize = currIdx - tknIdx;
+            char* newTkn = (char*)malloc(sizeof(char) * (tknSize + 1));
+            *(newTkn + tknSize) = 0;
+            char* newTknPtr = newTkn;
+
+            while(tknIdx != currIdx) {
+                *newTknPtr++ = input[tknIdx++];
+            }
+
+            *tokensPtr++ = newTkn; currSize++;
+
+            tknIdx = ++currIdx;
+
+            if(size == currSize) {
+                size *= 2;
+                tokens = (char**)realloc(tokens, sizeof(char*) * (size + 1));
+                tokensPtr = (char**)(tokens + currSize);
+            }
+        } else {
+            currIdx++;
+        }
+    }
+    *tokensPtr = NULL;
+
+    return tokens;
+}
+void freeTokens(char** tokens) {
+    char** tokensPtr = (char**)tokens;
+
+    while(*tokensPtr) {
+        free(*tokensPtr);
+    }
+    free(tokens);
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        target *= -1;
+        sign = -1;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    *(result + size) = 0;
+
+    int idx = endIdx;
+    while(idx >= startIdx) {
+        *(result + idx--) = (target % 10) + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == 0) {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
