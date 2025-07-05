b069c78c22f26644a10d23d7750cd467bfbf8e67
damhyeong <rhdwhdals8@naver.com>
2025-07-06 00:19:29 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.c
@@ -0,0 +1,216 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+
+    참고로, AI 작성 글이 아니라, 진짜 공식문서 뒤져가면서 힘들게 얻은 지식을 나눠보려고 합니다.
+*/
+
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+0.25 초 (추가 시간 없음)	128 MB	300495	97032	77941	31.765%
+
+문제
+---
+땅 위에 달팽이가 있다.
+
+이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
+
+달팽이는 낮에 A미터 올라갈 수 있다.
+
+하지만, 밤에 잠을 자는 동안 B미터 미끄러진다.
+
+또, 정상에 올라간 후에는 미끄러지지 않는다.
+
+달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
+
+입력
+---
+첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다.
+
+(1 ≤ B < A ≤ V ≤ 1,000,000,000)
+
+출력
+---
+첫째 줄에 달팽이가 나무 막대를 모두 올라가는데 며칠이 걸리는지 출력한다.
+
+예제 입력 1
+2 1 5
+예제 출력 1
+4
+예제 입력 2
+5 1 6
+예제 출력 2
+2
+예제 입력 3
+100 99 1000000000
+예제 출력 3
+999999901
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t byte);
+extern void free(void* memory);
+extern void* realloc(void* memory, size_t byte);
+
+char** tokenizer(const char* input);
+void freeTokens(char** tokens);
+
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+int main(void) {
+    char input[255];
+
+    fgets(input, sizeof(input), stdin);
+
+    char** tokens = (char**)tokenizer(input);
+    char** tokensPtr = (char**)tokens;
+
+    int A = parseInt(*tokensPtr++);
+    int B = parseInt(*tokensPtr++);
+    int V = parseInt(*tokensPtr++);
+
+    freeTokens(tokens);
+
+    int currDay = 1;
+
+    V -= A;
+
+    currDay += (V % (A - B) == 0) ? V / (A - B) : (V / (A - B)) + 1;
+
+    char* resultStr = intToStr(currDay);
+
+    fputs(resultStr, stdout); fputc('\n', stdout);
+
+    free(resultStr);
+
+    return 0;
+}
+
+
+char** tokenizer(const char* input) {
+    int size = 5;
+    int currSize = 0;
+
+    char** tokens = (char**)malloc(sizeof(char*) * (size + 1));
+    char** tokensPtr = (char**)tokens;
+
+    char* inputPtr = (char*)input;
+
+    int tknIdx = 0;
+    int currIdx = 0;
+
+    while(*inputPtr) {
+        char ch = *inputPtr++;
+
+        if(isBlank(ch) == 1) {
+            if(tknIdx == currIdx) {
+                tknIdx = ++currIdx;
+                continue;
+            }
+
+            int tknSize = currIdx - tknIdx;
+            char* newTkn = (char*)malloc(sizeof(char) * (tknSize + 1));
+            *(newTkn + tknSize) = 0;
+            char* newTknPtr = (char*)newTkn;
+
+            while(tknIdx != currIdx) {
+                *newTknPtr++ = input[tknIdx++];
+            }
+
+            *tokensPtr++ = newTkn; currSize++;
+
+            tknIdx = ++currIdx;
+
+            if(size == currSize) {
+                size *= 2;
+                tokens = (char**)realloc(tokens, sizeof(char*) * (size + 1));
+                tokensPtr = (char**)(tokens + currSize);
+            }
+        } else {
+            currIdx++;
+        }
+    }
+    *tokensPtr = NULL;
+
+    return tokens;
+}
+void freeTokens(char** tokens) {
+    char** tokensPtr = (char**)tokens;
+
+    while(*tokensPtr) {
+        free(*tokensPtr++);
+    }
+    free(tokens);
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        sign = -1;
+        target *= -1;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    *(result + size) = 0;
+
+    int idx = endIdx;
+    while(idx >= startIdx) {
+        *(result + idx--) = (target % 10) + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == 0) {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
