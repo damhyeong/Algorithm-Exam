2ffcb70c1674fce285dbc6f3151b5fb6711c1f4e
damhyeong <rhdwhdals8@naver.com>
2025-07-19 23:54:53 +0900

    [Silver V] Title: 영화감독 숌, Time: 116 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.c
@@ -0,0 +1,187 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+
+    참고로, AI 작성 글이 아니라, 진짜 공식문서 뒤져가면서 힘들게 얻은 지식을 나눠보려고 합니다.
+*/
+
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	128 MB	121846	73436	59213	59.883%
+문제
+666은 종말을 나타내는 수라고 한다. 따라서, 많은 블록버스터 영화에서는 666이 들어간 제목을 많이 사용한다. 영화감독 숌은 세상의 종말 이라는 시리즈 영화의 감독이다. 조지 루카스는 스타워즈를 만들 때, 스타워즈 1, 스타워즈 2, 스타워즈 3, 스타워즈 4, 스타워즈 5, 스타워즈 6과 같이 이름을 지었고, 피터 잭슨은 반지의 제왕을 만들 때, 반지의 제왕 1, 반지의 제왕 2, 반지의 제왕 3과 같이 영화 제목을 지었다. 하지만 숌은 자신이 조지 루카스와 피터 잭슨을 뛰어넘는다는 것을 보여주기 위해서 영화 제목을 좀 다르게 만들기로 했다.
+
+종말의 수란 어떤 수에 6이 적어도 3개 이상 연속으로 들어가는 수를 말한다. 제일 작은 종말의 수는 666이고, 그 다음으로 큰 수는 1666, 2666, 3666, .... 이다. 따라서, 숌은 첫 번째 영화의 제목은 "세상의 종말 666", 두 번째 영화의 제목은 "세상의 종말 1666"와 같이 이름을 지을 것이다. 일반화해서 생각하면, N번째 영화의 제목은 세상의 종말 (N번째로 작은 종말의 수) 와 같다.
+
+숌이 만든 N번째 영화의 제목에 들어간 수를 출력하는 프로그램을 작성하시오. 숌은 이 시리즈를 항상 차례대로 만들고, 다른 영화는 만들지 않는다.
+
+입력
+첫째 줄에 N이 주어진다. N은 10,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄에 N번째 영화의 제목에 들어간 수를 출력한다.
+
+예제 입력 1
+2
+예제 출력 1
+1666
+예제 입력 2
+3
+예제 출력 2
+2666
+예제 입력 3
+6
+예제 출력 3
+5666
+예제 입력 4
+187
+예제 출력 4
+66666
+예제 입력 5
+500
+예제 출력 5
+166699
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t byte);
+extern void free(void* memory);
+
+_Bool isEndNum(int number);
+
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+
+int main(void) {
+    char input[100];
+
+    fgets(input, sizeof(input), stdin);
+
+    int N = parseInt(input);
+
+    int idx = 1;
+
+    // 현재 몇 번째 종말 숫자인지
+    int currEndNumbers = 0;
+    // 바로 최근의 종말 숫자는 무엇인지
+    int currEndNumber = 0;
+
+    while(N != currEndNumbers) {
+        if(isEndNum(idx) == 1) {
+            currEndNumbers++;
+            currEndNumber = idx;
+        }
+
+        idx++;
+    }
+
+    char* result = (char*)intToStr(currEndNumber);
+
+    fputs(result, stdout); fputc('\n', stdout);
+
+    free(result);
+
+    return 0;
+}
+
+_Bool isEndNum(int number) {
+    char* strNum = intToStr(number);
+    char* strNumPtr = (char*)strNum;
+
+    _Bool isEndNumber = 0;
+
+    int currSixLen = 0;
+    char preCh = ' ';
+
+    while(*strNumPtr) {
+        char ch = *strNumPtr++;
+
+        if(ch == '6') {
+            currSixLen = (preCh == '6') ? currSixLen + 1 : 1;
+        } else {
+            currSixLen = 0;
+        }
+
+        preCh = ch;
+
+        if(currSixLen == 3) {
+            isEndNumber = 1;
+            break;
+        }
+    }
+
+    free(strNum);
+
+    return isEndNumber;
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        sign = -1;
+        target *= -1;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    *(result + size) = 0;
+
+    int idx = endIdx;
+    while(idx >= startIdx) {
+        *(result + idx--) = (target % 10) + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == 0) {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
