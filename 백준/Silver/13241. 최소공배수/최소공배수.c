03795e8d58c6e90e46a698911c427feb21acc065
damhyeong <rhdwhdals8@naver.com>
2025-08-05 00:08:13 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.c
@@ -0,0 +1,238 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+
+    참고로, AI 작성 글이 아니라, 진짜 공식문서 뒤져가면서 힘들게 얻은 지식을 나눠보려고 합니다.
+*/
+
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	512 MB	29998	18655	17059	62.849%
+문제
+정수 B에 0보다 큰 정수인 N을 곱해 정수 A를 만들 수 있다면, A는 B의 배수이다.
+
+예:
+
+10은 5의 배수이다 (5*2 = 10)
+10은 10의 배수이다(10*1 = 10)
+6은 1의 배수이다(1*6 = 6)
+20은 1, 2, 4,5,10,20의 배수이다.
+다른 예:
+
+2와 5의 최소공배수는 10이고, 그 이유는 2와 5보다 작은 공배수가 없기 때문이다.
+10과 20의 최소공배수는 20이다.
+5와 3의 최소공배수는 15이다.
+당신은 두 수에 대하여 최소공배수를 구하는 프로그램을 작성 하는 것이 목표이다.
+
+입력
+한 줄에 두 정수 A와 B가 공백으로 분리되어 주어진다.
+
+50%의 입력 중 A와 B는 1000(10^3)보다 작다. 다른 50%의 입력은 1000보다 크고 100000000(10^8)보다 작다.
+
+추가: 큰 수 입력에 대하여 변수를 64비트 정수로 선언하시오. C/C++에서는 long long int를 사용하고, Java에서는 long을 사용하시오.
+
+출력
+A와 B의 최소공배수를 한 줄에 출력한다.
+
+예제 입력 1
+1 1
+예제 출력 1
+1
+예제 입력 2
+3 5
+예제 출력 2
+15
+예제 입력 3
+1 123
+예제 출력 3
+123
+예제 입력 4
+121 199
+예제 출력 4
+24079
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t byte);
+extern void free(void* memory);
+extern void* realloc(void* memory, size_t byte);
+
+int max_divisor(int n1, int n2);
+
+char** tokenzier(const char* input);
+void freeTokens(char** tokens);
+
+char* intToStr(long long target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+int main(void) {
+    char input[255];
+
+    fgets(input, sizeof(input), stdin);
+
+    char** tokens = tokenzier(input);
+    char** tokensPtr = (char**)tokens;
+
+    int A = parseInt(*tokensPtr++);
+    int B = parseInt(*tokensPtr++);
+
+    freeTokens(tokens);
+
+    int small = A > B ? B : A;
+    int big = A > B ? A : B;
+
+    int max_div = max_divisor(small, big);
+
+    int tmp_mul = small / max_div;
+
+    long long min_mul = (long long)big * (long long)tmp_mul;
+
+    char* result = intToStr(min_mul);
+    fputs(result, stdout); fputc('\n', stdout);
+    free(result);
+
+    return 0;
+}
+
+int max_divisor(int n1, int n2) {
+    int tmpDiv = 1;
+    int maxDiv = -1;
+
+    while(tmpDiv <= n1 / 2 && tmpDiv <= n2 / 2) {
+        maxDiv = n1 % tmpDiv == 0 && n2 % tmpDiv == 0 ? tmpDiv : maxDiv;
+        tmpDiv++;
+    }
+
+    maxDiv = n1 % n2 == 0 ? n2 : maxDiv;
+    maxDiv = n2 % n1 == 0 ? n1 : maxDiv;
+
+    return maxDiv;
+}
+
+char** tokenzier(const char* input) {
+    int size = 5;
+    int currSize = 0;
+
+    char** tokens = (char**)malloc(sizeof(char*) * (size + 1));
+    char** tokensPtr = (char**)tokens;
+
+    char* inputPtr = (char*)input;
+
+    int tknIdx = 0;
+    int currIdx = 0;
+
+    while(*inputPtr) {
+        char ch = *inputPtr++;
+
+        if(isBlank(ch) == 1) {
+            if(tknIdx == currIdx) {
+                tknIdx = ++currIdx;
+                continue;
+            }
+
+            int tknSize = currIdx - tknIdx;
+            char* newTkn = (char*)malloc(sizeof(char) * (tknSize + 1));
+            *(newTkn + tknSize) = 0;
+            char* newTknPtr = (char*)newTkn;
+
+            while(tknIdx != currIdx) {
+                *newTknPtr++ = input[tknIdx++];
+            }
+
+            *tokensPtr++ = newTkn; currSize++;
+
+            tknIdx = ++currIdx;
+
+            if(size == currSize) {
+                size *= 2;
+                tokens = (char**)realloc(tokens, sizeof(char*) * (size + 1));
+                tokensPtr = (char**)(tokens + currSize);
+            }
+        } else {
+            currIdx++;
+        }
+    }
+    *tokensPtr = NULL;
+
+    return tokens;
+}
+void freeTokens(char** tokens) {
+    char** tokensPtr = (char**)tokens;
+
+    while(*tokensPtr) {
+        free(*tokensPtr++);
+    }
+    free(tokens);
+
+    return;
+}
+
+char* intToStr(long long target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        target *= -1;
+        sign = -1;
+    }
+
+    long long temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    *(result + size) = 0;
+
+    int idx = endIdx;
+    while(idx >= startIdx) {
+        *(result + idx--) = (target % 10) + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == 0) {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
