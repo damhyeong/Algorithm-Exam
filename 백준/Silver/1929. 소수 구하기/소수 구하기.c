df0cfa0a6b3e5301cc81127f03e13774995e4a90
damhyeong <rhdwhdals8@naver.com>
2025-08-06 00:37:58 +0900

    [Silver III] Title: 소수 구하기, Time: 188 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.c
@@ -0,0 +1,222 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+
+    참고로, AI 작성 글이 아니라, 진짜 공식문서 뒤져가면서 힘들게 얻은 지식을 나눠보려고 합니다.
+*/
+
+/**
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	256 MB	322379	99823	70156	28.813%
+
+문제
+---
+M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+
+입력
+---
+첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다.
+
+(1 ≤ M ≤ N ≤ 1,000,000) M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
+
+출력
+---
+한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
+
+예제 입력 1
+3 16
+예제 출력 1
+3
+5
+7
+11
+13
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t byte);
+extern void free(void* memory);
+extern void* realloc(void* memory, size_t byte);
+
+_Bool isPrimeNum(int target);
+
+char** tokenizer(const char* input);
+void freeTokens(char** tokens);
+
+char* intToStr(int target);
+int parseInt(const char* str);
+_Bool isBlank(char ch);
+
+int main(void) {
+    char input[255];
+
+    fgets(input, sizeof(input), stdin);
+
+    char** tokens = (char**)tokenizer(input);
+    char** tokensPtr = (char**)tokens;
+
+    int M = parseInt(*tokensPtr++);
+    int N = parseInt(*tokensPtr++);
+
+    freeTokens(tokens);
+
+    int idx = M;
+    while(idx <= N) {
+        _Bool isPrime = isPrimeNum(idx);
+
+        if(isPrime == 1) {
+            char* tmpResult = intToStr(idx);
+            fputs(tmpResult, stdout); fputc('\n', stdout);
+            free(tmpResult);
+        }
+
+        idx++;
+    }
+
+    return 0;
+}
+
+_Bool isPrimeNum(int target) {
+    if(target == 1) {
+        return 0;
+    } else if(target == 2 || target == 3) {
+        return 1;
+    }
+
+    int idx = 2;
+    _Bool isPrime = 1;
+    while(idx * idx <= target) {
+        if(target % idx == 0) {
+            isPrime = 0;
+            break;
+        }
+        idx++;
+    }
+
+    return isPrime;
+}
+
+char** tokenizer(const char* input) {
+    int size = 5;
+    int currSize = 0;
+
+    char** tokens = (char**)malloc(sizeof(char*) * (size + 1));
+    char** tokensPtr = (char**)tokens;
+
+    char* inputPtr = (char*)input;
+    int tknIdx = 0;
+    int currIdx = 0;
+
+    while(*inputPtr) {
+        char ch = *inputPtr++;
+
+        if(isBlank(ch) == 1) {
+            if(tknIdx == currIdx) {
+                tknIdx = ++currIdx;
+                continue;
+            }
+
+            int tknSize = currIdx - tknIdx;
+            char* newTkn = (char*)malloc(sizeof(char) * (tknSize + 1));
+            *(newTkn + tknSize) = 0;
+            char* newTknPtr = (char*)newTkn;
+
+            while(tknIdx != currIdx) {
+                *newTknPtr++ = input[tknIdx++];
+            }
+            *tokensPtr++ = newTkn; currSize++;
+
+            tknIdx = ++currIdx;
+
+            if(size == currSize) {
+                size *= 2;
+                tokens = (char**)realloc(tokens, sizeof(char*) * (size + 1));
+                tokensPtr = (char**)(tokens + currSize);
+            }
+        } else {
+            currIdx++;
+        }
+    }
+    *tokensPtr = NULL;
+
+    return tokens;
+}
+void freeTokens(char** tokens) {
+    char** tokensPtr = (char**)tokens;
+
+    while(*tokensPtr) {
+        free(*tokensPtr++);
+    }
+    free(tokens);
+
+    return;
+}
+
+char* intToStr(int target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        sign = -1;
+        target *= -1;
+    }
+
+    int temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    *(result + size) = 0;
+
+    int idx = endIdx;
+    while(idx >= startIdx) {
+        *(result + idx--) = (target % 10) + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+int parseInt(const char* str) {
+    int sign = 1;
+    int result = 0;
+
+    char* strPtr = (char*)str;
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == 0) {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
