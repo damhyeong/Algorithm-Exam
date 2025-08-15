226d28f4e8aade8cf1455d6be45b66bb18e298ed
damhyeong <rhdwhdals8@naver.com>
2025-08-15 19:32:26 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.c
@@ -0,0 +1,132 @@
+/**
+    @Author : 공담형
+    @Email : rhdwhdals8765@gmail.com
+    @Github : https://github.com/damhyeong
+    @Blog : https://codecreature.tistory.com
+
+    "최소한의" 기능과 라이브러리를 사용하고, 나머지 기능을 직접 구현하기 때문에,
+
+    다른 C 코드를 참조하시는 것이 좋습니다.
+
+    블로그에서 다양한 컴퓨터 관점과 Computer Science 를 다루고 있으니 궁금하면 들러보셔요!
+
+    참고로, AI 작성 글이 아니라, 진짜 공식문서 뒤져가면서 힘들게 얻은 지식을 나눠보려고 합니다.
+*/
+
+/**
+팩토리얼 성공
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	193558	108202	89342	56.149%
+
+문제
+---
+0보다 크거나 같은 정수 N이 주어진다.
+
+이때, N!을 출력하는 프로그램을 작성하시오.
+
+입력
+---
+첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+
+출력
+첫째 줄에 N!을 출력한다.
+
+예제 입력 1
+10
+예제 출력 1
+3628800
+예제 입력 2
+0
+예제 출력 2
+1
+*/
+
+#include<stdio.h>
+
+extern void* malloc(size_t byte);
+extern void free(void* memory);
+
+char* numToStr(long target);
+long parseNumber(const char* str);
+_Bool isBlank(char ch);
+
+int main(void) {
+    char input[100];
+
+    fgets(input, sizeof(input), stdin);
+
+    long N = parseNumber(input);
+
+    long result = 1;
+    long idx = 1;
+    while(idx <= N) {
+        result *= idx++;
+    }
+
+    char* resultStr = numToStr(result);
+    fputs(resultStr, stdout); fputc('\n', stdout);
+    free(resultStr);
+
+    return 0;
+}
+
+char* numToStr(long target) {
+    int sign = 1;
+    int len = 0;
+
+    if(target < 0) {
+        target *= -1;
+        sign = -1;
+    }
+
+    long temp = target;
+    do {
+        temp /= 10;
+        len++;
+    } while(temp != 0);
+
+    int size = sign == 1 ? len : len + 1;
+    int endIdx = size - 1;
+    int startIdx = sign == 1 ? 0 : 1;
+
+    char* result = (char*)malloc(sizeof(char) * (size + 1));
+    *(result + size) = 0;
+
+    int idx = endIdx;
+    while(idx >= startIdx) {
+        *(result + idx--) = (target % 10) + '0';
+        target /= 10;
+    }
+    if(sign == -1) {
+        *result = '-';
+    }
+
+    return result;
+}
+long parseNumber(const char* str) {
+    long result = 0;
+    long sign = 1;
+
+    char* strPtr = (char*)str;
+    if(*strPtr == '-') {
+        sign = -1;
+        strPtr++;
+    }
+
+    while(isBlank(*strPtr) != 1) {
+        result *= 10;
+        result += *strPtr++ - '0';
+    }
+
+    return result * sign;
+}
+_Bool isBlank(char ch) {
+    if(ch == 32 || ch == 0) {
+        return 1;
+    } else if(ch >= 9 && ch <= 13) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
